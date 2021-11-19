f7a35aea2205bfb6909ce5ab3a5366ca371d9884
Raquel <raquel.panapalen@gmail.com>
2021-11-19 19:18:22 +0100

    FIX: decodeURI not required & minor fixes

--- server2.0/routes/users.js
+++ server2.0/routes/users.js
@@ -1,6 +1,6 @@
 const getUser = async (db, query) => {
     try {
-        const userInfo = await db.collection('users').findOne({userid: query.userid})
+        const userInfo = await db.collection('users').findOne(query)
         return userInfo != null ? {
             id: userInfo.userid,
             username: userInfo.username
